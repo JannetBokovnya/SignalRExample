4e05efe500eb0ad22900f855d9c5421f2653f19c
Janet.Bokovnya <jannet.bokovnya@gmail.com>
2022-05-13 16:51:05 +0300

    new initial commit

---
+++ SignalRClient/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.AspNetCore.SignalR.Client;
+using System.Threading.Tasks;
+
+namespace SignalRClient
+{
+    class Program
+    {
+        static HubConnection hubConnection;
+        static async Task Main(string[] args)
+        {
+            hubConnection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:6771/notification")
+                .Build();
+
+            hubConnection.On<string>("Send", message => Console.WriteLine($"Message from server: {message}"));
+
+            await hubConnection.StartAsync();
+
+            bool isExit = false;
+
+            while (!isExit)
+            {
+                var message = Console.ReadLine();
+
+                if (message != "exit")
+                    await hubConnection.SendAsync("SendMessage", message);
+                else
+                    isExit = true;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
